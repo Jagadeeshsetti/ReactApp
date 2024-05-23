e351b6703ddedfdcf69b393a7b038310ccb5553e
unknown <jagadeeshsetti2001@gmail.com>
2024-05-23 23:05:41 +0530

    jsx, rules, export, import, fragments

--- src/App.js
+++ src/App.js
@@ -7,27 +7,66 @@ import FormComponent from "./components/forms/form.js"
 import TableComponent from "./components/tables/table.js"
 import  "./style.css"
 import ImageComponent from "./components/image/image.js"
-
+import Heading1, {Heading2,Heading3,Heading4,Heading5,Heading6 as SecendaryHeading} from "./components/headings/headings.js"
 
 function App(){
-  return(
+  // const employee={
+  //   name:"Jagadeesh", 
+  //   secondName:"vinay"
+  // }
+  // const {name,secondName}=employee
+  // return(
+  //   <div>
+
+  //     <h1>Hello World {name}</h1>
+  //     <h1>Hello World {secondName}</h1>
+      
+  //   </div>
+  // )
+
+
+  const isSubscribe=true
+
+  // if(isSubscribe){
+  //   return <ListComponent/>
+  // }
+  // else{
+  //   return <ImageComponent/>
+  // }
+
+  // return (
+  //   <div>
+  //     {
+  //       isSubscribe
+  //       ?
+  //       <ListComponent/>
+  //       :
+  //      <h1>Please subscribe</h1>
+  //     }
+  //   </div>
+  // )
+
+
+  // return (
+  //   <div>
+  //     {
+  //       isSubscribe  &&  <ListComponent/>
+    
+  //     }
+  //   </div>
+  // )
+
+   return(
     <div>
-      <h1>
-      <center>  Hello World</center>
-      </h1>
-      <Button />
-
-      <ListComponent />
-
-    <FormComponent />
-
-    <TableComponent />
-
-    <ImageComponent />
-
-    <h2>Login feature Completed</h2>
+    <Heading1/>
+    <Heading2/>
+    <Heading3/>
+    <Heading4/>
+    <Heading5/>
+    <SecendaryHeading/>
     </div>
-  )
+   )
+
 }
 
 export default App
