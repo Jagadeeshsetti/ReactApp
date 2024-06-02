be6e65b5a7dd77975f5af6b946da5b9db35d1e4d
Jagadeeshsetti <159634197+Jagadeeshsetti@users.noreply.github.com>
2024-06-02 13:28:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,71 +2,60 @@
 
 
 import Button from "./components/button/button.js"
-import ListComponent from "./components/list/list.js"
-import FormComponent from "./components/forms/form.js"
-import TableComponent from "./components/tables/table.js"
-import  "./style.css"
-import ImageComponent from "./components/image/image.js"
-import Heading1, {Heading2,Heading3,Heading4,Heading5,Heading6 as SecendaryHeading} from "./components/headings/headings.js"
+import List from "./components/list/list.js"
+import Form from "./components/forms/form.js"
+import Table from "./components/tables/table.js"
+import Image from "./components/image/image.js"
+import Heading1, {Heading2,Heading3,Heading4,Heading5,Heading6,Heading6 as SecendaryHeading} from "./components/headings/headings.js"
+import React from "react"
+
 
 function App(){
-  // const employee={
-  //   name:"Jagadeesh", 
-  //   secondName:"vinay"
-  // }
-  // const {name,secondName}=employee
-  // return(
-  //   <div>
-
-  //     <h1>Hello World {name}</h1>
-  //     <h1>Hello World {secondName}</h1>
-      
-  //   </div>
-  // )
-
-
-  const isSubscribe=true
-
-  // if(isSubscribe){
-  //   return <ListComponent/>
-  // }
-  // else{
-  //   return <ImageComponent/>
-  // }
-
-  // return (
-  //   <div>
-  //     {
-  //       isSubscribe
-  //       ?
-  //       <ListComponent/>
-  //       :
-  //      <h1>Please subscribe</h1>
-  //     }
-  //   </div>
-  // )
-
-
-  // return (
-  //   <div>
-  //     {
-  //       isSubscribe  &&  <ListComponent/>
+  
     
-  //     }
-  //   </div>
-  // )
+        const employees=[
+          {
+            name:"Srikanth",
+            role:"Full stack developer",
+            salary:200000
+          },
+          {
+            name:"sai naik",
+            role:"React developer",
+            salary:100000
+          },
+          {
+            name:"pavan",
+            role:"Angular developer",
+            salary:150000
+          },
+          {
+            name:"pavan",
+            role:"Angular developer",
+            salary:150000
+          }
+      ]
 
-   return(
-    <div>
-    <Heading1/>
-    <Heading2/>
-    <Heading3/>
-    <Heading4/>
-    <Heading5/>
-    <SecendaryHeading/>
-    </div>
-   )
-
+      return(
+        <>
+    
+        {
+          employees.map((eachEmployee,index)=>{
+            return(
+              <div>
+                <h2>{index+1}</h2>
+                <h3>{eachEmployee.name}</h3>
+                <h3>{eachEmployee.salary}</h3>
+              </div>
+            )
+          })
+        }
+        
+         
+    
+        </>
+      )
+    
 }
 
 export default App
