b3be1de4e9663cdb6b48dde4b30afcc254be5f44
Jagadeeshsetti <159634197+Jagadeeshsetti@users.noreply.github.com>
2024-06-06 11:31:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,40 @@
+import Accordion from 'react-bootstrap/Accordion';
+import List from '../list/list';
+import Heading1 from '../headings/headings';
+import Image from '../image/image';
 
-
-
-import Button from "./components/button/button.js"
-import ListComponent from "./components/list/list.js"
-import FormComponent from "./components/forms/form.js"
-import TableComponent from "./components/tables/table.js"
-import  "./style.css"
-import ImageComponent from "./components/image/image.js"
-import Heading1, {Heading2,Heading3,Heading4,Heading5,Heading6 as SecendaryHeading} from "./components/headings/headings.js"
-import React from "react"
-
-function App(){
-   const isSubscibed = false
-  
-   const employees=[
-    {
-      teamName:"SRH",
-      players:["a","b"],
-      trophy:[2016]
+function AccordionComponent () {
+    const ipiTeams = [{
+      teamName:"CSK",
+      player:["Rutguraj","Rachin","Rahene","Dubey","Dhoni","Jadeja","Chahar","Pathirana","Santnar","Pandya"]
     },
-    {
-      teamName:"Mumbai",
-      players:["a","b"],
-      trophy:[2016]
-    },
-   
-  ]
+  {  
+   teamName:"RCB",
+    player:["Kohli","Maxwal","Faf","Siraj","Lomron","Karthik","Jacks","Patidar","Green","Dayal"]
+  },
+  {  
+    teamName:"KKR",
+     player:["Iyer","Rinku","Salt","Starc","Narine","Russell","Rana","Gurbaz","Rahman","pandya"]
+   }    
+]
+  return (
+    <Accordion defaultActiveKey="0">
 
-
-   return(
-    <>
-   
     {
-      employees.map((eachEmployee,index)=>{
-        const{teamName,players,trophy} =eachEmployee
-                return(
-          <React.Fragment >
-            <h2>{index+1}</h2>
-            <h3>{teamName}</h3>
-            <h3>{players}</h3>
-            <h3>{trophy}</h3>
-          </React.Fragment>
+      ipiTeams.map((eachiplTeams, index)=>{
+        const{teamName, player}=eachiplTeams
+        return(
+          <>
+            <Accordion.Item eventKey={index}>
+              <Accordion.Header><b>{teamName}</b></Accordion.Header>
+              <Accordion.Body>{player}</Accordion.Body>
+            </Accordion.Item>
+          </>
         )
       })
     }
-
-    </>
-   )
+    </Accordion>
+  );
 }
 
-export default App
+export default  AccordionComponent
